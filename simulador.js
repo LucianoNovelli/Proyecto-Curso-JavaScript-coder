03eed524dbb70bc5873c1d43979d9f966791bade
Luciano Novelli <luciano_novelli94@hotmail.com>
2022-11-01 22:12:54 -0300

    se agrego storage y JSON

--- simulador.js
+++ simulador.js
@@ -2,27 +2,6 @@ const carro = [];
 
 let totalPrecio;
 
-/*
-console.log('*** Todos los Productos')
-productos.forEach(n => n.imprimir());
-
-console.log('*** Primer Producto con precio > 10000')
-productos.find(n => n.precio > 10000).imprimir()
-
-
-console.log('*** Todos los Productos con precio > 10000')
-productos.filter(n => n.precio > 10000).forEach(n => n.imprimir());
-
-
-console.log('*** Todos los Productos ordenados por precio ascendente')
-productos.sort((a,b) => (a.precio - b.precio)).forEach(n => n.imprimir());
-
-
-console.log('*** Todos los Productos ordenados por precio descendente')
-productos.sort((a,b) => b.precio - a.precio).forEach(n => n.imprimir());
-*/
-
-//Clases de DOM
 
 let Caja = document.getElementById ("caja");
 console.log(Caja.innerHTML);
@@ -32,62 +11,6 @@ let contendor = document.getElementsByTagName("h1");
 console.log(contendor[0].innerText);
 contendor[0].innerHTML = "Tienda Gamming";
 
-//tabla dinamica
-/*
-let tabla = document.createElement("tabla");
-
-tabla.className = "table table-striped"
-
-let tablaBody = document.createElement("tbody");
-
-for (const producto of productos){
-    tablaBody.innerHTML += `
-        <tr>
-            <td>${producto.nombre}</td>
-            <td>$ ${producto.precio}</td>
-            <td>${producto.caracteristicas}</td>
-            <td>${producto.codigo}</td>
-        </tr>
-`;
-}
-
-tabla.append (tablaBody);
-let CrearTabla =  document.getElementById("tablas");
-CrearTabla.append (tabla);
-*/
-
-//Ingreso de Cars
-
-
-/*
-let articulosCartas = document.getElementById ("cartas");
-
-for (const producto of productos){
-        let carta = document.createElement ("div");
-        carta.className = "card col-xl-2"
-        carta.innerHTML += `
-
-        <div class="card-body">
-            <img src=${producto.imagen} class="card-img-top" alt="...">
-            <h5 class="card-title">${producto.nombre}</h5>
-            <p class="card-text">$ ${producto.precioConIva()}</p>
-            <p class="card-text">${producto.caracteristicas}</p>
-            <button id = "btn${producto.codigo}" class="btn btn-primary">Comprar</button>
-        </div>
-        `;
-        articulosCartas.append(carta);
-}
-
-productos.forEach (producto =>{
-    document.getElementById(`btn${producto.codigo}`).addEventListener("click",function(){
-        agregarAlCarrito(producto)
-    })
-})
-*/
-
-
-
-
 // Funcion que retorna una lista con los html de cada carta
 function getHtmlCardsOfProductos(productos) {
     let htmlProductos = []
@@ -95,7 +18,7 @@ function getHtmlCardsOfProductos(productos) {
 
     for (const producto of productos){
         let carta = document.createElement ("div");
-        carta.className = "card col-xl-2 "
+        carta.className = "card col-xl-2 col-10 col-l-5"
     
         cuerpoCarta = document.createElement ("div");
         cuerpoCarta.className = "card-body "
@@ -131,7 +54,7 @@ for (const categoria of categorias){
     let articulos = document.getElementById("cartas")
 
     let cartas = document.createElement("div")
-    cartas.className = "cart row"
+    cartas.className = "cart row justify-content-center"
     cartas.innerHTML = `<h3>Nuestros ${categoria} </h3>`
     
     let productosFiltrados = productos.filter(p => p.caracteristicas == categoria)
@@ -143,7 +66,7 @@ for (const categoria of categorias){
 
 function agregarAlCarrito (productoComprado){
     carro.push (productoComprado);
-    console.table(carro);
+    //console.table(carro);
     alert("Agregaste "+productoComprado.nombre+" Al carrito");
     document.getElementById("tablabody").innerHTML += `
     <tr>
@@ -151,8 +74,34 @@ function agregarAlCarrito (productoComprado){
         <td>${productoComprado.nombre}</td>
         <td>${productoComprado.precio}</td>
     </tr>
-`;
-totalPrecio = carro.reduce((acumulador,producto)=> acumulador + producto.precio,0);
-let infoTotal = document.getElementById("total");
-infoTotal.innerText="Total a pagar $: "+totalPrecio;
-};
+    `;
+    totalPrecio = carro.reduce((acumulador,producto)=> acumulador + producto.precio,0);
+    let infoTotal = document.getElementById("total");
+    infoTotal.innerText="Total a pagar $ "+totalPrecio;
+
+
+ //Se agrega el carro al Storage
+
+    const guardarLocal = (clave, valor) => { localStorage.setItem(clave, valor) };
+
+    guardarLocal("listaProductos", JSON.stringify(carro));
+
+    class NuevoProducto{
+        constructor(obj){
+            this.nombre = obj.nombre.toUpperCase();
+            this.precio = parseFloat (obj.precio);
+        }
+    }
+
+    const almacenados = JSON.parse (localStorage.getItem ("listaProductos"));
+
+    const finalizarCompra = [];
+
+    for (const objeto of almacenados){
+
+        finalizarCompra.push (new NuevoProducto (objeto));
+    }
+
+    console.log(finalizarCompra);
+}
+
